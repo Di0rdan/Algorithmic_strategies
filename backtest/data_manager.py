9abdaf90bec1141706e8d9841f308069659a6fb0
Dmitriy Vorobev <opiopopdu@gmail.com>
2021-12-26 14:52:10 +0300

    init backtest

---
+++ backtest/data_manager.py
@@ -0,0 +1,8 @@
+class AbstractDataManager:
+
+    def __init__(self, stock_exchange, ticker, freq):
+        pass
+
+    def get(self, time):
+        pass
+
---
+++ backtest/metric.py
@@ -0,0 +1,10 @@
+class AbstractMetric:
+
+    def __init__(self):
+        pass
+
+    def update(self, time, real, predicted):
+        pass
+
+    def get(self, time):
+        pass
